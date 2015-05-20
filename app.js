b3714d64d1155b330a7034034b84e289cd6b6f53
Rose THERESA <rosetheresa@Roses-MacBook-Air.local>
2015-05-20 09:56:14 -0700

    make project page

---
+++ app.js
@@ -0,0 +1,56 @@
+//Add the ability to click on a todo item 
+//to toggle it completed. 
+//Can you make use of event bubbling?
+
+body = document.querySelector('body');
+input = document.createElement('input')
+button = document.createElement('button')
+div = document.createElement('div')
+ul = document.createElement('ul')
+
+input.placeholder  = "type your to-do task here"
+
+
+div.innerHTML = 'test';
+button.name = "submit"
+button.id = "submit"
+button.innerHTML = "click to slsubmit"
+
+body.appendChild(input)
+body.appendChild(button)
+body.appendChild(ul)
+
+button.addEventListener('mousedown',function(e) {
+console.log(e);
+
+li = document.createElement('li')
+li.innerHTML = input.value
+
+	ul.appendChild(li)
+
+	})
+
+input.addEventListener('keypress',function(e) {
+console.log(e);
+
+if(e.which === 13)
+{
+	li = document.createElement('li')
+	li.innerHTML = input.value
+	ul.appendChild(li)
+	input.value = ""
+
+}
+
+var x = this.value;
+console.log(x);
+	})
+
+ul.addEventListener("click", function(e){
+	if(e.srcElement.className){
+		e.srcElement.className = "";
+	}
+	else {
+		e.srcElement.className = "completed";
+	}
+});
